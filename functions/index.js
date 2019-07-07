202c3fc99faaa5be3f8f495eceabfc869df6d0c0
Jason Michaels <jason.michaels721@gmail.com>
2019-07-07 20:07:59 +0000

    config testing

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const functions = require("firebase-functions");
+const fire_env = (JSON.stringify(functions.config()) === '{}') ? require('./runtimeconfig.json') : functions.config()
 
 function one() {
   console.log("hello");
@@ -8,6 +9,10 @@ function one() {
 
 function two() {
   console.log("world");
+}
+
+function config() {
+ console.log(fire_env['my']['config'])
 }
 
 exports.app_config2 = functions.https.onRequest((request, response) => {
@@ -21,6 +26,7 @@ exports.app_config2 = functions.https.onRequest((request, response) => {
     } else {
       one();
       two();
+      config();
       response.send("Hello World (post)...");
     }
   }
